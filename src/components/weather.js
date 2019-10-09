2ff912f8b7de956d9f914fe7f2a9ba3b0d30d122
Kamilnr10 <kamil.nr10@gmail.com>
2019-10-09 20:02:29 +0200

    ProDone

--- src/components/weather.js
+++ src/components/weather.js
@@ -11,7 +11,7 @@ const Weather = props => {
           <h5>Państwo: {props.country} </h5>
         </div>
       </div>
-      <hr></hr>
+      <hr />
       <div>
         <p>Opis pogody: {props.description}</p>
       </div>
@@ -25,21 +25,21 @@ const Weather = props => {
       <div>
         <h5>
           Temperatura: {Math.round(props.temp)}
-          <span>&#176;C</span> <i class="fas fa-thermometer-half"></i>
+          <span>&#176;C</span> <i className="fas fa-thermometer-half"></i>
         </h5>
       </div>
-      <hr></hr>
+      <hr />
       <div className="row">
         <div className="col">
           <p>
             Szerokość geograficzna: {props.latitude}&#176;{" "}
-            <i class="fas fa-globe"></i>
+            <i className="fas fa-globe"></i>
           </p>
         </div>
         <div className="col">
           <p>
             Długość geograficzna: {props.longtitude}&#176;{" "}
-            <i class="fas fa-globe"></i>
+            <i className="fas fa-globe"></i>
           </p>
         </div>
       </div>
@@ -47,25 +47,25 @@ const Weather = props => {
         <div className="col">
           <h5>
             Wilgotność: {props.humidity}
-            <span>%</span> <i class="fas fa-tint"></i>
+            <span>%</span> <i className="fas fa-tint"></i>
           </h5>
         </div>
         <div className="col">
           <h5>Ciśnienie: {props.pressure}hPa</h5>
         </div>
       </div>
-      <hr></hr>
+      <hr />
       <h5>
         Temperatura min: {Math.round(props.tempMin)}
-        <span>&#176;C</span> <i class="fas fa-temperature-low"></i>
+        <span>&#176;C</span> <i className="fas fa-temperature-low"></i>
       </h5>
       <h5>
         Temperatura max: {Math.round(props.tempMax)}
-        <span>&#176;C</span> <i class="fas fa-temperature-high"></i>
+        <span>&#176;C</span> <i className="fas fa-temperature-high"></i>
       </h5>
       <h5>
         Wiatr: {props.wind} <span>km/h </span>
-        <i class="fas fa-wind"></i>
+        <i className="fas fa-wind"></i>
       </h5>
     </div>
   );
