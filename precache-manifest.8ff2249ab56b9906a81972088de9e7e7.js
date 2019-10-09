350c3f8d764b20c47f8fed7a27c2a975e41ca121
Kamilnr10 <kamil.nr10@gmail.com>
2019-10-09 20:18:26 +0200

    Updates

---
+++ precache-manifest.8ff2249ab56b9906a81972088de9e7e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55899375b29edcf2039899a9b062c4df",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "aa05d1db4a92b8527913",
+    "url": "/Weather-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "0eb3b05246ca2686e0ec",
+    "url": "/Weather-app/static/css/main.0efe4c96.chunk.css"
+  },
+  {
+    "revision": "aa05d1db4a92b8527913",
+    "url": "/Weather-app/static/js/2.e34a16bb.chunk.js"
+  },
+  {
+    "revision": "0eb3b05246ca2686e0ec",
+    "url": "/Weather-app/static/js/main.9a1c7cf6.chunk.js"
+  },
+  {
+    "revision": "61ffa2378a1ad046306f",
+    "url": "/Weather-app/static/js/runtime-main.5dedfbad.js"
+  }
+]);
